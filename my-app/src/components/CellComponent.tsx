a12d39758cf7d0d2e9d82f9f987f88ba5d086078
vanya <vanchello089@gmail.com>
2022-11-27 17:30:42 +0500

    add markup

---
+++ my-app/src/components/CellComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CellComponent = () => {
+    return (
+        <div
+            className="cell"
+        >
+
+        </div>
+    );
+};
+
+export default CellComponent;
